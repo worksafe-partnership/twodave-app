92245f93d2976801cf47a0ed9dc7524c85472909
chris <chris@evergreen.co>
2019-06-25 16:43:55 +0100

    added remaining yamls (untested) and other project files, added items to config

--- config/egc.php
+++ config/egc.php
@@ -59,4 +59,77 @@ return [
         'PC_AC' => 'Principle Contractor Amended',
         'PC_R' => 'Principle Contractor Rejected',
     ],
+    'hazard_who_risk' => [
+        'C' => 'Client',
+        'M' => 'Management',
+        'S' => 'Supervisor',
+        'E' => 'Employees',
+        'S' => 'Sub-contractor',
+        'T' => 'Third Party',
+        'P' => 'Public',
+        'R' => 'Residents',
+        'O' => 'Other - please specify'
+    ],
+    'methodology_categories' => [
+        'TEXT' => [
+            'label' => 'Text',
+            'view' => 'modules.company.project.methodology.text',
+            'pdf_view' => 'pdf.methodology.text',
+            'gfx/methodology/text.png',
+        ],
+        'TEXT_IMAGE' => [
+            'label' => 'Text and Image',
+            'view' => 'modules.company.project.methodology.text_image',
+            'pdf_view' => 'pdf.methodology.text_image',
+            'gfx/methodology/text_image.png',
+        ],
+        'SIMPLE_TABLE' => [
+            'label' => 'Simple Table',
+            'view' => 'modules.company.project.methodology.simple_table',
+            'pdf_view' => 'pdf.methodology.simple_table',
+            'gfx/methodology/simple_table.png',
+        ],
+        'COMPLEX_TABLE' => [
+            'label' => 'Complex Table',
+            'view' => 'modules.company.project.methodology.complex_table',
+            'pdf_view' => 'pdf.methodology.complex_table',
+            'gfx/methodology/complex_table.png',
+        ],
+        'PROCESS' => [
+            'label' => 'Process',
+            'view' => 'modules.company.project.methodology.process',
+            'pdf_view' => 'pdf.methodology.process',
+            'gfx/methodology/process.png',
+        ],
+        'ICON' => [
+            'label' => 'Icon',
+            'view' => 'modules.company.project.methodology.icon',
+            'pdf_view' => 'pdf.methodology.icon',
+            'gfx/methodology/icon.png',
+        ],
+    ],
+    'methodology_list' => [
+        'TEXT' => 'Text',
+        'TEXT_IMAGE' => 'Text and Image',
+        'SIMPLE_TABLE' => 'Simple Table',
+        'COMPLEX_TABLE' => 'Complex Table',
+        'PROCESS' => 'Process',
+        'ICON' => 'Icon',
+    ],
+    'first_last' => [
+        'FIRST' => 'First',
+        'LAST' => 'Last',
+    ],
+    'icons' => [
+        'hard-hat' => 'Hard Hat',
+        // ETC
+    ],
+    'icon_images' => [
+        'hard-hat' => 'gfx/icons/hard-hat.png',
+        // ETC
+    ],
+    'icon_types' => [
+        'MAIN' => 'Main',
+        'SUB' = 'Sub',
+    ],
 ];
