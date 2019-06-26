fe09a768bfbe06ef890ef6ee8594dbe527b153a4
chris <chris@evergreen.co>
2019-06-26 16:02:58 +0100

    started tweaking companies to look nice

--- config/structure/company/config.php
+++ config/structure/company/config.php
@@ -14,25 +14,19 @@ return [
     'controller' => 'CompanyController',
     'datatable' => [
         "columns" => [
-        'id' => ['visible' => false, 'searchable' => false, 'label' => 'Id'],
-        'name' => ['label' => 'Name'],
-        'review_timescale' => ['label' => 'Review Timescale'],
-        'vtrams_name' => ['label' => 'VTRAMs Name'],
-        'email' => ['label' => 'Contact Email'],
-        'phone' => ['label' => 'Phone Number'],
-        'fax' => ['label' => 'Fax Number'],
-        'low_risk_character' => ['label' => 'Low Risk Label'],
-        'med_risk_character' => ['label' => 'Medium Risk Label'],
-        'high_risk_character' => ['label' => 'High Risk Label'],
-        'no_risk_character' => ['label' => 'No Risk Label'],
-        'primary_colour' => ['label' => 'Primary Colour'],
-        'secondary_colour' => ['label' => 'Secondary Colour'],
-        'light_text' => ['label' => 'Light Text'],
-        'accept_label' => ['label' => 'Accept Label'],
-        'amend_label' => ['label' => 'Amend Label'],
-        'reject_label' => ['label' => 'Reject Label'],
-        'logo' => ['label' => 'Logo']
-    ]
+            'id' => ['visible' => false, 'searchable' => false, 'label' => 'Id'],
+            'logo' => [
+                'label' => 'Logo',
+                'raw' => true,
+            ],
+            'name' => ['label' => 'Name'],
+            'email' => ['label' => 'Contact Email'],
+            'phone' => ['label' => 'Phone Number'],
+            'fax' => ['label' => 'Fax Number'],
+            'primary_colour' => ['label' => 'Primary Colour'],
+            'vtrams_name' => ['label' => 'VTRAMs Name'],
+            'review_timescale' => ['label' => 'Review Timescale'],
+        ]
     ],
     'sidebar' => [],
     'permissions' => true
