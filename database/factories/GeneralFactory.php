87db155a977af9e131ea2c7f20dde2c0891ad0ef
Martin Bampton <martin@evergreen.co>
2019-06-28 14:37:26 +0100

    Initial Seeding

---
+++ database/factories/GeneralFactory.php
@@ -0,0 +1,64 @@
+<?php
+
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(App\Company::class, function (Faker $faker) {
+    $config = Config('egc');
+
+    return [
+        'name' => $faker->company,
+        'review_timescale' => $faker->randomElement(array_flip($config['review_timescales'])),
+        'vtrams_name' => 'TBC',
+        'email' => $faker->safeEmail,
+        'phone' => 'TBC',
+        'fax' => 'TBC',
+        'low_risk_character' => 'TBC',
+        'med_risk_character' => 'TBC',
+        'high_risk_character' => 'TBC',
+        'no_risk_character' => 'TBC',
+        'primary_colour' => $faker->hexcolor,
+        'secondary_colour' => $faker->hexcolor,
+        'light_text' => $faker->randomElement([null, 1]),
+        'accept_label' => 'Accept Label Test',
+        'amend_label' => 'Amend Label Test',
+        'reject_label' => 'Reject Label Test',
+    ];
+});
+
+$factory->define(App\User::class, function (Faker $faker) {
+    // $config = Config('egc');
+    return [
+        'name' => $faker->name,
+        'company_id' => 1,
+        'email' => $faker->safeEmail,
+        'password' => 'aaaaaaa'
+    ];
+});
+
+$factory->define(App\Project::class, function (Faker $faker) {
+    $config = Config('egc');
+
+    return [
+        'name' => $faker->name,
+        'ref' => $faker->randomNumber,
+        'company_id' => 1, // should always be overridden
+        'project_admin' => 1, // should always be overridden
+        'principle_contractor' => 1, // should always be overridden
+        'principle_contractor_name' => $faker->name,
+        'principle_contractor_email' => $faker->safeEmail,
+        'client_name' => $faker->name,
+        'review_timescale' => $faker->randomElement(array_flip($config['review_timescales'])),
+        'show_contact' => $faker->randomElement([null, 1]),
+    ];
+});
--- database/seeds/CompanySeeder.php
+++ database/seeds/CompanySeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
 
 class CompanySeeder extends Seeder
@@ -11,6 +12,51 @@ class CompanySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $counts = [
+            'extraCompanies' => 5
+        ];
+
+        $hash = new Bhash();
+        $suffix = env("EGC_PROJECT_NAME", "egl");
+        $prefix = "egWelding";
+        $password = $hash->make($prefix.$suffix);
+
+        // require_once base_path().'/vendor/fzaninotto/faker/src/autoload.php';
+        // $faker = Faker\Factory::create('en_GB');
+
+        $starterCompanies = ['UK FM Services', 'Woodford Heating and Energy', 'Berkeley Homes'];
+        $usersAndRoles = [
+            'Contracts Manager' => 2,
+            'Project Admin' => 3,
+            'Supervisor' => 4,
+        ];
+
+        foreach ($starterCompanies as $companyName) {
+            $company = factory(App\Company::class)->create(['name' => $companyName]);
+            foreach ($usersAndRoles as $role => $roleId) {
+                $users = factory(User::class, 2)->create([
+                    'name' => $company->name." - ".$role,
+                    'company_id' => $company->id,
+                    'password' => $password
+                ]);
+
+                foreach ($users as $user) {
+                    $user = User::find($user->id);
+                    $user->roles()->attach([$roleId]);
+                }
+            }
+
+            $projectAdmin = User::join('role_users', 'role_users.user_id', '=', 'users.id')
+                ->where('company_id', $company->id)
+                ->where('role_id', 4)
+                ->first();
+
+            $projects = factory(App\Project::class, 5)->create([
+                'company_id' => $company->id,
+                'project_admin' => $projectAdmin->id
+            ]);
+        }
+
+        // $seededCompanies = factory(App\Company::class, $counts['extraCompanies'])->create();
     }
 }
