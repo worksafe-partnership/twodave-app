a4ce8879218f568b61aa283c71f970231917fe64
Martin Bampton <martin@evergreen.co>
2019-07-03 13:17:43 +0100

    Pending and Rejected Dashboard Datatables for Company Admin users

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -53,10 +53,64 @@ class DashboardController extends Controller
             'data' => $submitted,
         ];
 
-        $this->customValues['tables'][] = [
-            'heading' => 'Submitted By You',
-            'table-id' => 'test_2',
-            'data' => $submitted,
-        ];
+        if ($role == 'company_admin') {
+            $pending = Vtram::where('status', "PENDING")
+                            ->when($user->company_id !== null, function ($q) use ($user) {
+                                $q->where('company_id', '=', $user->company_id);
+                            })
+                            ->whereHas('submitted', function ($roleQuery) {
+                                $roleQuery->whereHas('roles', function ($sub) {
+                                    $sub->whereIn('slug', ['contract_manager', 'project_admin', 'supervisor']);
+                                });
+                            })
+                            ->get([
+                                // 'company name', // "leave for now" - CP
+                                // 'plan number', // "leave for now" - CP
+                                'name',
+                                'status',
+                                'created_by',
+                                'submitted_by',
+                                'submitted_date',
+                                'approved_date',
+                                'approved_by',
+                                'review_due'
+                            ]);
+
+            $this->customValues['tables'][] = [
+                'heading' => 'Pending',
+                'table-id' => 'pending',
+                'data' => $pending,
+            ];
+
+            $rejected = Vtram::whereIn('status', ["EXTERNAL_REJECT", "REJECTED"])
+                            ->when($user->company_id !== null, function ($q) use ($user) {
+                                $q->where('company_id', '=', $user->company_id);
+                            })
+                            // ->whereHas('submitted', function ($roleQuery) {
+                            //     $roleQuery->whereHas('roles', function ($sub) {
+                            //         $sub->whereIn('slug', ['contract_manager', 'project_admin', 'supervisor']);
+                            //     });
+                            // })
+                            ->get([
+                                // 'company name', // "leave for now" - CP
+                                // 'plan number', // "leave for now" - CP
+                                'name',
+                                'status',
+                                'created_by',
+                                'submitted_by',
+                                'submitted_date',
+                                'approved_date',
+                                'approved_by',
+                                'review_due'
+                            ]);
+
+            $this->customValues['tables'][] = [
+                'heading' => 'Rejected',
+                'table-id' => 'rejected',
+                'data' => $rejected,
+            ];
+        }
+
+
     }
 }
