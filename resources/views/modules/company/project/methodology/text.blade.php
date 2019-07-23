27b2bdf35291962fdc2286fe8df998dd85110a20
Martin Bampton <martin@evergreen.co>
2019-07-23 16:37:02 +0100

    Updating text blade for methodologies

--- resources/views/modules/company/project/methodology/text.blade.php
+++ resources/views/modules/company/project/methodology/text.blade.php
@@ -1,1 +1,23 @@
-Text blade to go here.
+<div class="columns">
+    <div class="column">
+        <p class="sub-heading">Details</p>
+        <div class="field">
+            {{ EGForm::text('title', [
+                'label' => 'Title',
+                'value' => '',
+                'type' => $pageType
+            ]) }}
+        </div>
+    </div>
+</div>
+<div class="columns">
+    <div class="column">
+        <div class="field">
+            {{ EGForm::ckeditor('content', [
+                'label' => 'Content',
+                'value' => '',
+                'type' => $pageType
+            ]) }}
+        </div>
+    </div>
+</div>
