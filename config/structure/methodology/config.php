0c43430302be9fe48cc83e37398babed1145246d
Martin Bampton <martin@evergreen.co>
2019-07-26 11:59:02 +0100

    Removing datatables from hazards and methodology configs - not needed

--- config/structure/methodology/config.php
+++ config/structure/methodology/config.php
@@ -12,19 +12,6 @@ return [
     ],
     'icon' => 'list',
     'controller' => 'MethodologyController',
-    'datatable' => [
-        "columns" => [
-            'id' => ['visible' => false, 'searchable' => false, 'label' => 'Id'],
-            'category' => ['label' => 'Category'],
-            'entity' => ['label' => 'entity'],
-            'entity_id' => ['label' => 'entity_id'],
-            'text_before' => ['label' => 'Text Before'],
-            'text_after' => ['label' => 'Text After'],
-            'image' => ['label' => 'Image'],
-            'image_on' => ['label' => 'Image On'],
-            'list_order' => ['label' => 'list_order']
-        ]
-    ],
     'permissions' => true,
     'exclude_routes' => [
         'create',
