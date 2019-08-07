bfd0fb4c06b5415d3528c173857aefd3db99a0e9
Martin Bampton <martin@evergreen.co>
2019-08-07 16:56:49 +0100

    Typo in email, updating formatting.

--- resources/views/emails/principle_contractor.blade.php
+++ resources/views/emails/principle_contractor.blade.php
@@ -1,6 +1,7 @@
 <p>Dear {{ $project->principle_contractor_name }},<p>
 <p>One or more VTRAMS are available for review, please click on the link below to access the VTRAMS</p>
 <a href="{{ $link }}">Access my VTRAMS</a>
-<p>Some email clients have trouble with link, if you're experiancing any issues please copy the following into a web browser: {{ $link }}</p>
+<p>Some email clients have trouble with link. If you're experiencing any issues please copy the following into a web browser:</p>
+<p>{{ $link }}</p>
 <p>Kind regards</p>
 <p>The Worksafe Partnership</p>
