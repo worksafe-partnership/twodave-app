84e3431ad052a7525fefa7516d6d04670bb8a48d
Martin Bampton <martin@evergreen.co>
2019-08-21 10:50:04 +0100

    Permanently deleting a unique link when the last relevant VTRAMs is deleted

--- app/Http/Controllers/CompanyVtramController.php
+++ app/Http/Controllers/CompanyVtramController.php
@@ -18,6 +18,7 @@ use App\TableRow;
 use App\NextNumber;
 use App\Instruction;
 use App\Methodology;
+use App\UniqueLink;
 use App\Http\Classes\VTLogic;
 use Illuminate\Http\Request;
 use App\Http\Requests\VtramRequest;
@@ -560,5 +561,20 @@ class CompanyVtramController extends Controller
         ];
         parent::_buildProperties($args);
         return parent::_renderView("layouts.custom");
+    }
+
+
+    public function permanentlyDeleted($record)
+    {
+        $project = Project::withTrashed()->findOrFail($record->project_id);
+        $vtramsCount = VTram::join('projects', 'vtrams.project_id', '=', 'projects.id')
+                       ->where('vtrams.status', 'AWAITING_EXTERNAL')
+                       ->where('principle_contractor', 1)
+                       ->where('principle_contractor_email', $project->principle_contractor)
+                       ->count();
+
+        if ($vtramsCount == 0) {
+            UniqueLink::where('email', $project->principle_contractor_email)->delete();
+        }
     }
 }
